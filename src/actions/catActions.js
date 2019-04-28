ff3e29dbcabb5c7d95ece1c5a6c1a307b422f28d
Kevin Peery <kevin.peery@gmail.com>
2019-04-27 19:00:08 -0500

    Done.

---
+++ src/actions/catActions.js
@@ -0,0 +1,20 @@
+// export const fetchCats = () => dispatch => {
+//   dispatch({ type: "LOADING_CATS" });
+//   fetch("http://localhost:4000/db")
+//     .then(response => response.json())
+//     .then(responseJSON =>
+//       dispatch({ type: "FETCH_CATS", payload: responseJSON.images })
+//     );
+// };
+
+
+export function fetchCats() {
+  return (dispatch) => {
+    dispatch({ type: 'LOADING_CATS' });
+    return fetch('http://localhost:4000/db')
+      .then(response => response.json())
+      .then(responseJson => {
+        dispatch({ type: 'FETCH_CATS', payload: responseJson.images })
+      })
+  }
+}
