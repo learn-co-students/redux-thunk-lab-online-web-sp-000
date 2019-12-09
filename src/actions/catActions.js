7717234aeac20e8511c86d8f1024a55e15448b75
milenaonmaui <milenakari@yahoo.com>
2019-12-09 13:34:04 -0800

    Done.

--- src/actions/catActions.js
+++ src/actions/catActions.js
@@ -1,1 +1,16 @@
 // write and export your action creator function here
+
+export const fetchCats = () => {
+    //this is the function being returned
+    return (dispatch) => {
+        //first return a dispatch that we are loading cats
+        dispatch({type: 'LOADING_CATS'})
+        // then start fetch
+        fetch('https://learn-co-curriculum.github.io/cat-api/cats.json')
+        .then(response => {
+            return response.json()
+        }).then(responseJSON => {
+            dispatch({type: 'ADD_CATS', cats: responseJSON.images})
+        })
+    }
+}
