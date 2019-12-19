575348acb471bc1a247a4f7d360291a59f7d4283
Anya <anya.s.litvinova@gmail.com>
2019-12-19 17:38:21 -0500

    Done.

--- src/CatList.js
+++ src/CatList.js
@@ -1,1 +1,15 @@
-// write your CatList component here
+import React from 'react'
+
+const CatList = props => {
+	function renderCats() {
+		return props.catPics.map((pic) => <li key={pic.id}><img src={pic.url} alt='kittens'/></li>)
+	}
+
+	return (
+		<div>
+			{renderCats()}
+		</div>
+	)
+}
+
+export default CatList
