1f7e79bdd8dee2cb7d1a6465de4f2b0682eb4951
Tommy Curley <thomas.curley93@gmail.com>
2020-03-23 13:20:14 -0600

    Done.

--- src/CatList.js
+++ src/CatList.js
@@ -3,7 +3,14 @@ import React, { Component } from 'react';
 
 class CatList extends Component {
 
-  catPics = () => this.props.catPics.map((catPic) => <div><img alt={catPic.id} src={catPic.url}/></div>)
+  // catPics = () => this.props.catPics.map((catPic) => <div><img alt={catPic.id} src={catPic.url}/></div>)
+  catPics() {
+      if (this.props.loading) {
+        return <div><h1>Loading...</h1></div>
+      } else {
+        return this.props.catPics.map((catPic) => <div><img alt={catPic.id} src={catPic.url}/></div>)
+      }
+  }
 
   render() {
     return(
