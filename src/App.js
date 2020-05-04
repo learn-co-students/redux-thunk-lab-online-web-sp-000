d1064cbe2c3084bae219e73dfc82d5d3ed89570c
agraniero <4wowbagger2@gmail.com>
2020-05-04 15:47:03 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,39 @@
 import React, { Component } from 'react';
+import CatList from './CatList';
+import { connect } from 'react-redux';
+import { fetchCats } from './actions/catActions';
 
-class App extends Component {   
+class App extends Component { 
+  
+  
+  componentDidMount() {
+    this.props.fetchCats()
+  }
+
+  loading = () => {
+    if(this.props.loading){
+      return <div>loading...</div>
+    } else {
+      return <CatList catPics={this.props.catPics} />
+    }
+  }
   
   render() {
     return (
       <div>
         <h1>CatBook</h1>
-        {/* add CatList component here */}
+        {this.loading()}
       </div>
     );
   }
 }
 
-export default App
+const mapDispatchToProps = state => {
+  return {
+    catPics: state.cats,
+    loading: state.loading
+  }
+}
+
+export default connect(mapDispatchToProps, { fetchCats })(App)
 
--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
 import App from './App'
+import thunk from 'redux-thunk'
+import { createStore, applyMiddleware } from 'redux'
+import catsReducer from './reducers/catsReducer'
+import { Provider } from 'react-redux'
 
+const store = createStore(catsReducer, applyMiddleware(thunk))
 
 ReactDOM.render(
-    <App />,
+    <Provider store={store}>
+        <App />
+    </Provider>,
     document.getElementById('root')
 );
