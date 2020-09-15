b91216a09901f72184e73cce726cdb2044b572af
l1mk <jhonxlink@hotmail.com>
2020-09-14 20:40:22 -0500

    Done.

--- src/CatList.js
+++ src/CatList.js
@@ -1,1 +1,26 @@
-// write your CatList component here
+import React, { Component } from 'react';
+
+class CatList extends Component {
+
+
+  render() {
+    console.log('0 - rendering catlist', this.props.catPics)
+    if (this.props.loading === false ){
+    return(
+      <div>
+          {console.log('2 - cats loaded', this.props.catPics)}
+          {this.props.catPics.map((element, index) => <img key={index} source={element.url} alt={element.id}></img>)}
+      </div>
+    );
+  } else {
+    return(
+        <div>
+            {console.log('1 - wait cats are loading', this.props.catPics)}
+        Cats are loading
+        </div>
+      );
+  }
+  }
+};
+
+export default CatList;
