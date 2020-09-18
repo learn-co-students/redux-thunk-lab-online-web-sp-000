cc8bca3d91e5586e1ed0720253358ce08c27d175
mbade1 <mbade314@gmail.com>
2020-09-18 17:07:20 -0500

    Done.

--- src/CatList.js
+++ src/CatList.js
@@ -11,7 +11,7 @@ import React from 'react';
 
 const CatList = ({catPics}) => {
     
-    const renderCatPics = catPics.map(catPic => <img src={catPic.source_url}/>)
+    const renderCatPics = catPics.map(catPic => <img src={catPic.url}/>)
     return (
         <div>
             {renderCatPics}
