96f9151ef4f554f247f5b4653c28ab753cc015d2
Gabrielkq <gabrielkutik@yahoo.com>
2020-11-12 12:49:28 -0500

    Done.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,34 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import {fetchCats} from './actions/catActions';
+import CatList from './CatList';
 
 class App extends Component {   
+
+  componentDidMount(){
+    console.log("did mount:", this.props)
+    this.props.fetchCats()
+  }
   
   render() {
+    console.log("in render=", this.props)
     return (
       <div>
         <h1>CatBook</h1>
-        {/* add CatList component here */}
+      {/* add CatList component here */}
+          <CatList catPics={this.props.catPics} /> 
+
       </div>
     );
   }
 }
 
-export default App
+const msp = state => {
+  return{
+    catPics: state.cats,
+    loading: state.loading
+  }
+}
+
+export default connect(msp, {fetchCats})(App)
 
--- src/actions/catActions.js
+++ src/actions/catActions.js
@@ -1,1 +1,14 @@
 // write and export your action creator function here
+
+const url = 'https://learn-co-curriculum.github.io/cat-api/cats.json'
+
+export const fetchCats = () => {
+    return (dispatch) => {
+        dispatch({type: 'LOADING_CATS'})
+        fetch(url)
+        .then(res => res.json())
+        .then(data => {
+            dispatch({type: 'ADD_CATS', cats: data.images})
+        })
+    }
+} 
