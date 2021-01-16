328b6e7a6ac67e43b5471eff9083d57a39b82fdb
KBENBENECK <kbenbeneck@gmail.com>
2021-01-16 00:40:16 -0500

    Done.

--- src/reducers/catsReducer.js
+++ src/reducers/catsReducer.js
@@ -1,1 +1,24 @@
-// add the catsReducer
+//3 add the catsReducer, switch statement that handles two action types
+
+const catsReducer = (
+    state = { cats: [], loading: false }, 
+    action) => {
+        switch(action.type) {
+            case 'LOADING_CATS':
+                return {
+                    ...state,
+                    cats: [...state.cats],
+                    loading: true 
+                }
+            case 'ADD_CATS':
+                return {
+                    ...state,
+                    cats: action.cats,
+                    loading: false
+                }
+            default:
+                return state;
+        }
+}
+export default catsReducer;
+
