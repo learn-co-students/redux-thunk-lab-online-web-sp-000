14b93b8bc40bb6289b41e574081e80bec56a7b5d
khoinguyenkc <khoinguyenkc@gmail.com>
2021-05-03 14:52:45 -0700

    Done.

--- src/CatList.js
+++ src/CatList.js
@@ -1,1 +1,22 @@
 // write your CatList component here
+import React, { Component } from 'react';
+
+
+
+
+export default class CatList extends Component {
+
+  renderCats = () => {
+    return this.props.catPics.map( catObj => { return <img src={catObj.url} /> })
+  }
+
+  render() {
+    return (
+
+      <div>
+      {this.renderCats()}
+      </div>
+
+    )
+  }
+};
--- src/actions/catActions.js
+++ src/actions/catActions.js
@@ -1,1 +1,32 @@
-// write and export your action creator function here
+// // write and export your action creator function here
+// const fetchCats = () => {
+//   return ( (dispatch) => {
+//
+//     // dispatch message that it's requesting/loading
+//     dispatch({type: 'LOADING_CATS'})
+//     // fetch request, once done, dispatch data
+//     fetch('https://learn-co-curriculum.github.io/cat-api/cats.json').then(
+//       resp => resp.json() ).then(
+//         json => dispatch({
+//       type: 'ADD_CATS',
+//       cats: json.images
+//       })
+//     )
+//
+//
+//     }
+//   )
+// };
+//
+// export default fetchCats;
+
+ export const fetchCats = () => {
+  return (dispatch) => {
+    dispatch({ type: 'LOADING_CATS'})
+    fetch('https://learn-co-curriculum.github.io/cat-api/cats.json').then(response => {
+      return response.json()
+    }).then(responseJSON => {
+      dispatch({ type: 'ADD_CATS', cats: responseJSON.images })
+    })
+  }
+}
