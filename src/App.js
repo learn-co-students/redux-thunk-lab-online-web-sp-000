a7dd5b4a9e14af9f71167f58cda5c21e3d9d7be0
deliaconstantino <dconstan92@gmail.com>
2021-06-15 11:19:45 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,44 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { fetchCats } from "./actions/catActions";
+import CatList from "./CatList";
 
-class App extends Component {   
-  
+class App extends Component {
+  componentDidMount() {
+    console.log(this.props);
+    this.props.fetchCats();
+  }
+
   render() {
+    console.log(this.props.catpics);
+    console.log("loading state: ", this.props.loading);
     return (
       <div>
         <h1>CatBook</h1>
-        {/* add CatList component here */}
+        {this.props.loading && (
+          <div>
+            <h3>loading...</h3>
+          </div>
+        )}
+        <CatList catPics={this.props.catpics} />
       </div>
     );
   }
 }
 
-export default App
+const mapStateToProps = (state) => {
+  return {
+    catpics: state.cats,
+    loading: state.loading,
+  };
+};
 
+const mapDispatchToProps = (dispatch) => {
+  return {
+    fetchCats: () => dispatch(fetchCats()),
+    //above is basicallly like a variable pointing to a function expression?
+    // why is this call a dispatch and the other two are as well?
+  };
+};
+// export default App
+export default connect(mapStateToProps, mapDispatchToProps)(App);
