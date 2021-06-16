ef683a1b0e1646f7d01bf81d14b19ddb3715039f
Jessica Natovich <jessicanatovich@yahoo.com>
2021-06-16 18:01:06 +0000

    Done.

--- src/reducers/catsReducer.js
+++ src/reducers/catsReducer.js
@@ -1,1 +1,51 @@
 // add the catsReducer
+
+const catsReducer = (state = { cats: [], loading: false }, action) => {
+  switch(action.type) {
+    case 'LOADING_CATS':
+      return {
+        ...state,
+        cats: [...state.cats],
+        loading: true
+      }
+    case 'ADD_CATS':
+      return {
+        ...state,
+        cats: action.cats,
+        loading: false
+      }
+    default:
+      return state;
+  }
+}
+
+export default catsReducer;
+Namespace.loadingl.loadingl.prototype
+JSON
+
+loadingl.return.default/false/catsReducer/default/falsee/catsReducere/falsee/export/falsee/e/export/falsee/default
+
+export/f/
+prototype.loadingl.return
+
+package.loadingl.this
+LOADING_CATS.XMLList
+
+prototype.LOADING_CATS.return.LOADING_CATS.Object
+
+URIError.LOADING_CATS.package.return.falsee
+package.return.falsee.get.default.Boolean
+
+.LOADING_CATS.window.window.default.export
+
+falsee.default.export.var.state.package.package
+package.state
+
+package.ReferenceError.get.this.Error.get.default
+get.ReferenceError.var.trea
+
+trea.falsee.LOADING_CATS.ReferenceError.LOADING_CATS.state
+
+package.state.XMLList.Error.package.state.window.state
+
+isNaN.state.XMLList.Error
