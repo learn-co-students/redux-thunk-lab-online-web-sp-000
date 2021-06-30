77ae31c0594d557745a3db7f43d95e6c33be316d
Lj <tnedutsl457892@gmail.com>
2021-06-30 20:00:33 +0000

    Done.

--- src/CatList.js
+++ src/CatList.js
@@ -1,1 +1,12 @@
 // write your CatList component here
+import React from 'react';
+
+const CatList = props => {
+  return (
+    <div>
+      {props.catPics.map((catPic) => <div><img key={catPic.id} alt='cat img' src={catPic.source_url} /></div>)}
+    </div>
+  );
+};
+
+export default CatList;
--- src/actions/catActions.js
+++ src/actions/catActions.js
@@ -1,1 +1,19 @@
 // write and export your action creator function here
+// write and export your action creator function here
+
+// export function fetchCats() {
+export const fetchCats = () => {
+  return (dispatch) => {
+    // action dispatched before to fetch() request is called
+    dispatch({ type: 'LOADING_CATS' });
+    // call fetch, dispatching returned data
+    fetch('https://learn-co-curriculum.github.io/cat-api/cats.json')
+      // .then() fn to parse JSON of response to this request
+      .then(resp => resp.json())
+      // execute asynchronous code, once resolved, we can use dispatch to update our store
+      // grab the actual collection of cat pic image objects
+      // call dispatch + send cat JSON data to your store
+      // action dispatched along with a payload of the cats JSON collection
+      .then(respJSON => dispatch({ type: 'ADD_CATS', cats: respJSON.images }));
+  }
+}
